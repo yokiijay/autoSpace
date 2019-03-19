bc52bb9725bae72702b01d8a0327a20686a3b87c
Xmac <xmaczone@Xmac.local>
2019-03-19 22:30:55 +0800

    2019-03-19 22:30:54

--- README.md
+++ README.md
@@ -1,42 +1,18 @@
-为什么我就是能这样娴熟地加上空格呢？
+# 改自(https://github.com/zizhengwu/daft-auto-spacing)[https://github.com/zizhengwu/daft-auto-spacing]
 =================
+## Api使用说明 
+```
+const str = '这行应该大约有40个字这行应该大约有40个字这行应该大约有40个字这行应该大约有40个字
+本行有35字本行有35字本行有35字本行有35字本行有35字本行有35字本行有35字
+到这一行就只有30个字到这一行就只有30个字到这一行就只有30个字
+In this line有33个字In this line有33个字In this line有33个字啊啊啊'
 
-有研究显示，打字的时候不喜欢在中文和英文之间加空格的人，感情路都走得很辛苦，有七成的比例会在 34 岁的时候跟自己不爱的人结婚，而其余三成的人最后只能把遗产留给自己的猫。毕竟爱情跟书写都需要适时地留白。与大家共勉之。[1]
-
-如果真正在意 typography，那么汉字与西文之间应该插入一个半角空格。Adobe InDesign、Microsoft Word 等对中文与西文混合排版支持较好的软件，都默认增大汉字和西文的间距。[2]
-
-汉学家称这个空白字元为「盘古之白」，因为它劈开了全形字和半形字之间的混沌，增加了文字的 readability。[1]
-
-如果你是一个虔诚的「空格党」，可以使用这个简单的工具，每当 Plain Text 的场景（例如电子邮件与 V2EX 发帖），在所有的汉字与拉丁字母间添加一个空格。如果原本已经手动添加了空格，这个工具并不会在这之间再添加额外的空格。
-
-项目地址：https://zizhengwu.github.io/daft-auto-spacing/  
-GitHub 地址：https://github.com/zizhengwu/daft-auto-spacing/
-
-[1]. 為什麼你們就是不能加個空格呢？: https://github.com/vinta/paranoid-auto-spacing  
-[2]. 中英文混排时中文与英文之间是否要有空格？: http://www.zhihu.com/question/19587406/answer/12298128  
-[3]. text-autospace.js: https://github.com/mastermay/text-autospace.js
-
-
-License
-=================
-The MIT License
-
-Copyright (c) 2014-2014 Zizheng Wu
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+const autoSpace = new AutoSpace()
+const output = autoSpace.spacing(str,32,48,'##') //参数1传入字符串，2传入最小值，3传入最大值，4传入插入的字符串
+console.log(output)
+<!-- 这行应该大约有 40 个字这行应该大约有 40 个字##这行应该大约有 40 个字这行应该大约有 40 个字
+本行有 35 字本行有 35 字本行有 35 字本行有##35 字本行有 35 字本行有 35 字本行有 35 字
+到这一行就只有 30 个字到这一行就只有 30 个字到这一行就只有 30 个字
+In this line 有 33 个字 In this li##ne 有 33 个字 In this line 有 33 个字啊啊啊 -->
+<!-- 将str的数字、英文前后补空格，并且每行如果字数在32-48个之间(英文数字算半个)就在中间插入## -->
+```
--- text-autospace.js/text-autospace.js
+++ text-autospace.js/text-autospace.js
@@ -1,3 +1,63 @@
+function AutoSpace() {
+    let unicode = []
+    unicode['latin'] = ['[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]'];
+    unicode['punc'] = ['[@&=_\,\.\?\!\$\%\^\*\-\+\/]', '[\(\\[\'"<‘“]', '[\)\\]\'">”’]'];
+    unicode['hanzi'] = ['[\u4E00-\u9FFF]', '[\u3400-\u4DB5\u9FA6-\u9FBB\uFA70-\uFAD9\u9FBC-\u9FC3\u3007\u3040-\u309E\u30A1-\u30FA\u30FD\u30FE\uFA0E-\uFA0F\uFA11\uFA13-\uFA14\uFA1F\uFA21\uFA23-\uFA24\uFA27-\uFA29]', '[\uD840-\uD868][\uDC00-\uDFFF]|\uD869[\uDC00-\uDEDF]', '\uD86D[\uDC00-\uDF3F]|[\uD86A-\uD86C][\uDC00-\uDFFF]|\uD869[\uDF00-\uDFFF]', '\uD86D[\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1F]', '[\u31C0-\u31E3]'];
+    unicode['biaodian'] = ['[·・︰、，。：；？！—ー⋯…．·／]', '[「『（〔【《〈“‘]', '[」』）〕】》〉’”]'];
+    unicode['zhuyin'] = [];
+    unicode['zhuyin'][0] = '[\u3105-\u312D\u31A0-\u31BA]';
+    unicode['zhuyin']['shengmu'] = '[\u3105-\u3119\u312A-\u312C\u31A0-\u31A3]';
+    unicode['zhuyin']['jieyin'] = '[\u3127-\u3129]';
+    unicode['zhuyin']['yunmu'] = '[\u311A-\u3126\u312D\u31A4-\u31B3\u31B8-\u31BA]';
+    unicode['zhuyin']['yunjiao'] = '[\u31B4-\u31B7]';
+    unicode['zhuyin']['diao'] = '[\u02D9\u02CA\u02C5\u02C7\u02CB\u02EA\u02EB]';
+
+    var unicode_set = function(set) {
+        var join = (set.match(/[hanzi|latin]/)) ? true : false;
+        var result = (join) ? unicode[set].join('|') : unicode[set];
+        return result;
+    };
+
+    this.spacing = (str,min,max,insert)=>{
+        let hanzi = unicode_set('hanzi');
+        let latin = unicode_set('latin') + '|' + unicode['punc'][0];
+        let punc = unicode['punc'];
+        let patterns = ['/(' + hanzi + ')(' + latin + '|' + punc[1] + ')/ig', '/(' + latin + '|' + punc[2] + ')(' + hanzi + ')/ig'];
+        let text = ''
+
+        const wrapStr = str.replace(/\n/g,'@wrap@')
+        let arrStr = wrapStr.split(/@wrap@/g)
+        let newStr = arrStr.map((val,index)=>{
+            console.log(val.length)
+            if(val.gblen() > min*2 && val.gblen() < max*2){
+                const strIndex = Math.round(val.length/2)
+                const newStr = val.substring(0,strIndex)+`${insert=insert?insert:'\#\#'}`+val.substring(strIndex)
+                return newStr
+            }else{
+                return val
+            }
+        }).join('\n')
+        
+        patterns.forEach(function(exp) {
+            newStr = newStr.replace(eval(exp), '$1 $2');
+        },
+        this);
+        return newStr
+    }
+}
+
+String.prototype.gblen = function() {    
+    var len = 0;    
+    for (var i=0; i<this.length; i++) {    
+        if (this.charCodeAt(i)>127 || this.charCodeAt(i)==94) {    
+             len += 2;    
+         } else {    
+             len ++;    
+         }    
+     }    
+    return len;    
+}
+
 // var unicode = [];
 
 // unicode['latin'] = ['[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]'];
@@ -35,40 +95,3 @@
 
 //     dom2.value = text;
 // };
-
-function AutoSpace(str,range) {
-    let unicode = []
-    unicode['latin'] = ['[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]'];
-    unicode['punc'] = ['[@&=_\,\.\?\!\$\%\^\*\-\+\/]', '[\(\\[\'"<‘“]', '[\)\\]\'">”’]'];
-    unicode['hanzi'] = ['[\u4E00-\u9FFF]', '[\u3400-\u4DB5\u9FA6-\u9FBB\uFA70-\uFAD9\u9FBC-\u9FC3\u3007\u3040-\u309E\u30A1-\u30FA\u30FD\u30FE\uFA0E-\uFA0F\uFA11\uFA13-\uFA14\uFA1F\uFA21\uFA23-\uFA24\uFA27-\uFA29]', '[\uD840-\uD868][\uDC00-\uDFFF]|\uD869[\uDC00-\uDEDF]', '\uD86D[\uDC00-\uDF3F]|[\uD86A-\uD86C][\uDC00-\uDFFF]|\uD869[\uDF00-\uDFFF]', '\uD86D[\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1F]', '[\u31C0-\u31E3]'];
-    unicode['biaodian'] = ['[·・︰、，。：；？！—ー⋯…．·／]', '[「『（〔【《〈“‘]', '[」』）〕】》〉’”]'];
-    unicode['zhuyin'] = [];
-    unicode['zhuyin'][0] = '[\u3105-\u312D\u31A0-\u31BA]';
-    unicode['zhuyin']['shengmu'] = '[\u3105-\u3119\u312A-\u312C\u31A0-\u31A3]';
-    unicode['zhuyin']['jieyin'] = '[\u3127-\u3129]';
-    unicode['zhuyin']['yunmu'] = '[\u311A-\u3126\u312D\u31A4-\u31B3\u31B8-\u31BA]';
-    unicode['zhuyin']['yunjiao'] = '[\u31B4-\u31B7]';
-    unicode['zhuyin']['diao'] = '[\u02D9\u02CA\u02C5\u02C7\u02CB\u02EA\u02EB]';
-
-    var unicode_set = function(set) {
-        var join = (set.match(/[hanzi|latin]/)) ? true : false;
-        var result = (join) ? unicode[set].join('|') : unicode[set];
-        return result;
-    };
-
-    this.spacing = ()=>{
-        let hanzi = unicode_set('hanzi');
-        let latin = unicode_set('latin') + '|' + unicode['punc'][0];
-        let punc = unicode['punc'];
-        let patterns = ['/(' + hanzi + ')(' + latin + '|' + punc[1] + ')/ig', '/(' + latin + '|' + punc[2] + ')(' + hanzi + ')/ig'];
-        let text = ''
-
-        patterns.forEach(function(exp) {
-            str = str.replace(eval(exp), '$1 $2');
-        },
-        this);
-
-        return str
-    }
-
-}
