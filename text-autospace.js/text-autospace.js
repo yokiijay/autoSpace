9618dc86035592f5340400444735aa990aa4cae2
Xmac <xmaczone@Xmac.local>
2019-03-22 00:20:27 +0800

    complete

--- text-autospace.js/text-autospace.js
+++ text-autospace.js/text-autospace.js
@@ -1,5 +1,5 @@
 function AutoSpace() {
-    let unicode = []
+    var unicode = []
     unicode['latin'] = ['[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]'];
     unicode['punc'] = ['[@&=_\,\.\?\!\$\%\^\*\-\+\/]', '[\(\\[\'"<‘“]', '[\)\\]\'">”’]'];
     unicode['hanzi'] = ['[\u4E00-\u9FFF]', '[\u3400-\u4DB5\u9FA6-\u9FBB\uFA70-\uFAD9\u9FBC-\u9FC3\u3007\u3040-\u309E\u30A1-\u30FA\u30FD\u30FE\uFA0E-\uFA0F\uFA11\uFA13-\uFA14\uFA1F\uFA21\uFA23-\uFA24\uFA27-\uFA29]', '[\uD840-\uD868][\uDC00-\uDFFF]|\uD869[\uDC00-\uDEDF]', '\uD86D[\uDC00-\uDF3F]|[\uD86A-\uD86C][\uDC00-\uDFFF]|\uD869[\uDF00-\uDFFF]', '\uD86D[\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1F]', '[\u31C0-\u31E3]'];
@@ -19,36 +19,85 @@ function AutoSpace() {
         var join = (set.match(/[hanzi|latin]/)) ? true : false;
         var result = (join) ? unicode[set].join('|') : unicode[set];
         return result;
-    };
+    }
 
-    this.spacing = (str,insert,min,max,puncList,biaodianList)=>{
-        max = max ? max : Number.POSITIVE_INFINITY
+    // space-god操作
+    this.space = (str,oneSpace)=>{
         let hanzi = unicode_set('hanzi');
         let latin = unicode_set('latin') + '|' + unicode['punc'][0];
         let punc = unicode['punc'];
         let patterns = ['/(' + hanzi + ')(' + latin + '|' + punc[1] + ')/ig', '/(' + latin + '|' + punc[2] + ')(' + hanzi + ')/ig'];
-
-
-        // space-god操作
+    
         patterns.forEach(function(exp) {
             str = str.replace(eval(exp), '$1 $2');
         },
         this);
 
-        // 转半角
-        str = this.enAngelTransfer(this.numAngelTransfer(str))
-        // 去punc
-        str = this.puncBye(str,puncList)
-        // 去标点
-        str = this.biaodianBye(str,biaodianList)
+        str = oneSpace ? str.replace( /( )+/g,' ' ) : str
 
-        // 插入insert 
-            /* 
-                1. 判断字数
-                2. 字数>min 切割成两半 : 字数<min 不作处理
-                3. 取右边那半 判断第一个单词是否是Latin (排除数字)
-                4. 是Latin 第一个空格处插入insert字段 : 不是Latin 在开头插入insert字段
-            */
+        return str
+    }
+
+    // 全角数字转半角
+    this.numAngelTransfer = str=>{
+        let quanNum = '０１２３４５６７８９'
+        for(let i=0;i<10;i++){
+            str = str.replace(new RegExp(quanNum.charAt(i),'g'),`${i.toString(16)}`)
+        }
+        return str
+    }
+
+    // 全角英文转半角
+    this.enAngelTransfer = str=>{
+        let quanEn = 'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＷＺ'
+        let banEn = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+        for(let i=0;i<quanEn.length;i++){
+            str = str.replace(new RegExp(quanEn.charAt(i),'g'),banEn.charAt(i))
+        }
+        return str
+    }
+
+    // 去punc
+    this.puncBye = (str, list)=>{
+        let punc = /[@&=_\,\.\;\{\}~`\#\|\:?\!\%\^\*\-\+\/\(\['"<‘“\)\]\'">”’]/
+        if(!list){
+            str = str.replace( new RegExp(punc,'g'),'' )/* .replace( /( )+/g,' ' ) */
+        }else{
+            for(let i=0;i<list.length;i++){
+                str = str.replace( new RegExp(`\\${list.charAt(i)}`,'g'),'' )/* .replace( /( )+/g,' ' ) */
+            }
+        }
+        return str
+    }
+
+    // 去标点
+    this.biaodianBye = (str, list, oneSpace)=>{
+        let biaodian = /[·・︰、，。：；？！—ー⋯…．·／「『（〔【《〈“‘」』）〕】》〉’”]+/
+        if(!list){
+            str = str.replace( new RegExp(biaodian,'g'),' ' )
+        }else{
+            for(let i=0;i<list.length;i++){
+                if(oneSpace && typeof oneSpace==='boolean'){
+                    str = str.replace( new RegExp(`\\${list.charAt(i)}| +`,'g'),' ' )
+                }else {
+                    str = str.replace( new RegExp(`\\${list.charAt(i)}`,'g'),' ' )
+                }
+            }
+        }
+        return str
+    }
+
+    // 插入insert 
+        /* 
+            1. 判断字数
+            2. 字数>min 切割成两半 : 字数<min 不作处理
+            3. 取右边那半 判断第一个单词是否是Latin (排除数字)
+            4. 是Latin 第一个空格处插入insert字段 : 不是Latin 在开头插入insert字段
+        */
+    this.insert = (str,insert,min,max)=>{
+        max = max || Number.POSITIVE_INFINITY
+        insert = insert || ''
         const wrapStr = str.replace(/\n/g,'@wrap@')
         let arrStr = wrapStr.split(/@wrap@/g)
         str = arrStr.map((val,index)=>{
@@ -75,63 +124,25 @@ function AutoSpace() {
         return str
     }
 
-    // 全角数字转半角方法
-    this.numAngelTransfer = num=>{
-        let quanNum = '０１２３４５６７８９'
-        for(let i=0;i<10;i++){
-            num = num.replace(new RegExp(quanNum.charAt(i),'g'),`${i.toString(16)}`)
-        }
-        return num
-    }
-
-    // 全角英文转半角办法
-    this.enAngelTransfer = en=>{
-        let quanEn = 'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＷＺ'
-        let banEn = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-        for(let i=0;i<quanEn.length;i++){
-            en = en.replace(new RegExp(quanEn.charAt(i),'g'),banEn.charAt(i))
-        }
-        return en
-    }
-
-    // 去punc
-    this.puncBye = (str, list)=>{
-        let punc = /[@&=_\,\.\;\{\}~`\#\|\:?\!\%\^\*\-\+\/\(\['"<‘“\)\]\'">”’]/
-        if(!list){
-            str = str.replace( new RegExp(punc,'g'),'' ).replace( /( )+/g,' ' )
-        }else{
-            for(let i=0;i<list.length;i++){
-                str = str.replace( new RegExp(`\\${list.charAt(i)}`,'g'),'' ).replace( /( )+/g,' ' )
-            }
-        }
-        return str
-    }
-    // 去标点
-    this.biaodianBye = (str, list)=>{
-        let biaodian = /[·・︰、，。：；？！—ー⋯…．·／「『（〔【《〈“‘」』）〕】》〉’”]/
-        if(!list){
-            str = str.replace( new RegExp(biaodian,'g'),' ' )
-        }else{
-            for(let i=0;i<list.length;i++){
-                str = str.replace( new RegExp(`\\${list.charAt(i)}`,'g'),' ' )
-            }
-        }
-        return str
-    }
-}
-
-String.prototype.gblen = function() {    
-    var len = 0;    
-    for (var i=0; i<this.length; i++) {    
-        if (this.charCodeAt(i)>127 || this.charCodeAt(i)==94) {    
-             len += 2;    
-         } else {    
-             len ++;    
+    // 计算字符个数 如 中文两个字符 英文一个
+    String.prototype.gblen = function() {    
+        var len = 0;    
+        for (var i=0; i<this.length; i++) {    
+            if (this.charCodeAt(i)>127 || this.charCodeAt(i)==94) {    
+                 len += 2;    
+             } else {    
+                 len ++;    
+             }    
          }    
-     }    
-    return len;    
+        return len;    
+    }
 }
+
+
+
+
+
+
 
 // var unicode = [];
 
